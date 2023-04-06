84cc52407e67b5c33cbf3b82155a973e6052ef52
chief <chiefpro@yandex.ru>
2023-04-06 18:17:29 +0300

    ext555

---
+++ examlpe05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Как вас зовут? ");
+string username = Console.ReadLine();
+if (username.ToLower() == "вася")
+    {
+        Console.WriteLine("Привет Васек!!!!");
+    }
+    else
+    {
+        Console.WriteLine("привет " + username);
+    }
