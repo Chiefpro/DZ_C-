c20882205e6f2b7d35a863a5423aa60939be1160
chief <chiefpro@yandex.ru>
2023-04-06 20:31:52 +0300

    fomated

--- example05/Program.cs
+++ example05/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.Write("Как вас зовут? ");
 string username = Console.ReadLine();
 if (username.ToLower() == "вася")
-    {
-        Console.WriteLine("Привет Васек!!!!");
-    }
-    else
-    {
-        Console.WriteLine("привет " + username);
-    }
+{
+    Console.WriteLine("Привет Васек!!!!");
+}
+else
+{
+    Console.WriteLine("привет " + username);
+}
