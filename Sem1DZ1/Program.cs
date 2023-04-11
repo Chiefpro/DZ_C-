5835915248bf9937969d5fbba643800ce682a050
chief <chiefpro@yandex.ru>
2023-04-11 17:32:39 +0300

    zadanie 4

--- Sem1DZ1/Program.cs
+++ Sem1DZ1/Program.cs
@@ -8,3 +8,25 @@ else
 {
     Console.WriteLine("B больше чем A");
 }
+// -------------- Задача 4 ------------
+Console.Write("Введите первое число ");
+string as2 = Console.ReadLine() ?? "";
+int a2 = int.Parse(as2);
+Console.Write("Введите второе число ");
+string bs2 = Console.ReadLine() ?? "";
+int b2 = int.Parse(bs2);
+Console.Write("Введите третье число ");
+string cs2 = Console.ReadLine() ?? "";
+int c2 = int.Parse(cs2);
+if (a2 > b2 && a2 > c2)
+{
+    Console.WriteLine("Максималье число первое");
+}
+else if (b2 > a2 && b2 > c2)
+{
+    Console.WriteLine("Максимальер число второе");
+}
+else
+{
+    Console.WriteLine("Максимальное число третье");
+}
