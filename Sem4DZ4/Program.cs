84ed5f83ec1fffd101089c7bd98a3d62e5842f51
chief <chiefpro@yandex.ru>
2023-04-22 23:54:36 +0300

    dz4

---
+++ Sem4DZ4/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.Clear();
+int z25a = Vvod("Введите число А ", "Ошибка ввода!!");
+int z25b = Vvod("введите число B ", "Ошибка ввода!!");
+int z25 = Zadacha25(z25a, z25b);
+Print($"{z25a}, {z25b} => {z25}");
+int z27a = Vvod("Введи число ", "error!");
+int z27 = Zadacha27(z27a);
+Print($"{z27a} => {z27}");
+int z29 = Vvod("введи степень массива ", "Error!!!");
+Zadacha29(z29);
+
+#region 
+static int Vvod(string text, string error)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int var))
+            return var;
+        Console.WriteLine(error);
+    }
+}
+static void Print(string text)
+{
+    Console.WriteLine(text);
+}
+
+static int Zadacha25(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+static int Zadacha27(int a)
+{
+    int result = 0;
+    int st = 0;
+    int wr = a;
+    int b = a;
+    while (b != 0)
+    {
+        b = b / 10;
+        st++;
+    }
+    for (int i = 0; i < st; i++)
+    {
+        result = result + (wr % 10);
+        wr = wr / 10;
+        Console.WriteLine(wr);
+
+        //Console.WriteLine(wr);
+
+    }
+    return result;
+}
+static void Zadacha29(int z)
+{
+    int[] array = new int[z];
+    for (int i = 0; i < z; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    for (int i = 0; i < z; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+#endregion
---
+++ Seminar04/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.Clear();
+//int z24 = Vvod("Введите целое число ", "Не корректно введено число!!!");
+//Zadacha24(z24);
+//int z26 = Vvod("введи число ", "Error1");
+//Zadacha26(z26);
+//int z28 = Vvod("vvedi ", "errore");
+//Zadacha28(z28);
+Zadacha30();
+
+#region 
+static int Vvod(string text, string error)
+{
+    int w;
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out w))
+            break;
+        Console.WriteLine(error);
+    }
+    return w;
+}
+
+static void Zadacha24(int chislo)
+{
+    int res = 0;
+    for (int i = 0; i <= chislo; i++)
+    {
+        res = res + i;
+    }
+    Console.WriteLine(res);
+}
+
+static void Zadacha26(int chiclo)
+{
+    int i = 0;
+    while (true)
+    {
+        chiclo = chiclo / 10;
+        i++;
+        if (chiclo == 0) break;
+    }
+    Console.WriteLine(i);
+}
+
+static void Zadacha28(int chislo)
+{
+    int f = 1;
+    for (int i = 1; i <= chislo; i++)
+    {
+        f *= i;
+    }
+    Console.WriteLine(f);
+}
+
+static void Zadacha30()
+{
+    int[] arr = new int[8];
+    for (int i = 1; i < 8; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    for(int i = 0; i<8; i++)
+    Console.Write($"{arr[i]} ");
+}
+#endregion
