be511485cc1d2f2b050211560aea0ebfce947340
chief <chiefpro@yandex.ru>
2023-04-23 22:09:07 +0300

    sem5.1

--- Seminar05/Program.cs
+++ Seminar05/Program.cs
@@ -4,8 +4,11 @@
 // int n = Negativ(array);
 // Console.WriteLine();
 // Console.WriteLine($"сумма положительных = {p}, сумма отрицательных = {n}");
-int[] arr_z31 = Zadacha31(12,-9,9);
-Zadacha32(arr_z31);
+// int[] arr_z31 = Zadacha31(12,-9,9);
+// Zadacha32(arr_z31);
+int[] arr_z33 = Zadacha31(12, -9, 9);
+int n = Vvod("введи число для нахождения ", "Error");
+Zadacha33(arr_z33, n);
 
 
 #region 
@@ -13,22 +16,45 @@ static int[] Zadacha31(int dlina, int min, int max)
 {
     int[] arr = new int[dlina];
     for (int i = 0; i < dlina; i++)
-        arr[i] = new Random().Next(min, max+1);
+        arr[i] = new Random().Next(min, max + 1);
     Console.WriteLine(String.Join(" ", arr));
     return arr;
 }
 static void Zadacha32(int[] arr)
 {
-    for(int i = 0; i<arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-            arr[i] *= -1;
+        arr[i] *= -1;
     }
     Console.WriteLine(String.Join(" ", arr));
+}
+static void Zadacha33(int[] arr, int num)
+{
+    bool nbb = false;
+    foreach (int el in arr)
+    {
+        if (el == num)
+        {
+            nbb = true;
+        }
+    }
+    if (nbb == true) Console.WriteLine("Da");
+    else Console.WriteLine("Net");
+}
+static int Vvod(string text, string error)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int var))
+            return var;
+        Console.WriteLine(error);
+    }
 }
 static int Pozitiv(int[] arr)
 {
     int res = 0;
-    foreach(int i in arr)
+    foreach (int i in arr)
     {
         if (i > 0) res += i;
     }
