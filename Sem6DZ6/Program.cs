720f521e5833a1366233c1c9490b3b2352be8e59
chief <chiefpro@yandex.ru>
2023-05-01 21:59:27 +0300

    sem7.0

--- Sem6DZ6/Program.cs
+++ Sem6DZ6/Program.cs
@@ -2,10 +2,10 @@
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
-// int d41 = Vvod("Ввевдите количество цифр ", "Error!!!");
-// int[] array41 = VvodArray(d41);
-// Console.WriteLine(String.Join(" ", array41));
-// Console.WriteLine($"Число положительных чисел => {FindPolozhitelnie(array41)}");
+int d41 = Vvod("Ввевдите количество цифр ", "Error!!!");
+int[] array41 = VvodArray(d41);
+Console.WriteLine(String.Join(" ", array41));
+Console.WriteLine($"Число положительных чисел => {FindPolozhitelnie(array41)}");
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
@@ -35,7 +35,7 @@ static int Vvod(string text, string error)
 static int[] VvodArray(int dlina)
 {
     int[] arr = new int[dlina];
-    int p = 0;
+    //int p = 0;
     for (int i = 0; i < arr.Length; i++)
         while (true)
         {
--- Seminar07/Program.cs
+++ Seminar07/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int[,] arr48 = NewArray(6, 5);
+PrintArray(arr48);
+Console.WriteLine();
+int[,] arr49 = FindNecet(arr48);
+PrintArray(arr49);
+int res51 = Diagolal(arr49);
+Console.WriteLine($"Сумма по диагонали => {res51}");
+
+
+
+
+#region 
+
+static int[,] NewArray(int a, int b)
+{
+    int[,] arr = new int[a, b];
+    for (int i = 0; i < a; i++)
+        for (int j = 0; j < b; j++)
+            arr[i, j] = i + j;
+    return arr;
+}
+static void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+static int[,] FindNecet(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+
+            if (arr[i, j] % 2 != 0)
+                // arr[i, j] = arr[i, j] * arr[i, j];
+                arr[i, j] = (int)Math.Pow(arr[i, j], 2);
+        }
+    }
+    return arr;
+}
+static int Diagolal(int[,] arr)
+{
+    int resalt = 0;
+    int d = 0;
+    if (arr.GetLength(0) > arr.GetLength(1)) d = arr.GetLength(1);
+    else d = arr.GetLength(0);
+    for (int i = 0; i < d; i++)
+    {
+        resalt += arr[i, i];
+    }
+    return resalt;
+}
+
+
+#endregion
