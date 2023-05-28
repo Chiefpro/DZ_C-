08d55cc2b043151b627d93b066b67b69d326fa4e
Chiefpro <chiefpro@yandex.ru>
2023-05-28 22:52:08 +0300

    sem9.0

---
+++ Seminar09/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region
+
+static int Vvod(string text, string error)
+{
+    while (true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out int var))
+            return var;
+        else
+            Console.WriteLine(error);
+    }
+}
+
+
+#endregion
